da529dde1583622b22d8dc0da19051f79239d866
Gnlow <gnlow@protonmail.com>
2024-03-22 14:46:57 +0900

    1181 fail

--- 1181.cpp
+++ 1181.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
+template <typename T>
+bool include(vector<T> v, T x) {
+    for (int i=0; i<v.size(); i++) {
+        if (x == v[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
     int n;
     cin >> n;
-
-    vector<int> v;
+    
+    vector<string> v;
 
     for (int i=0; i<n; i++) {
-        int x;
-        cin >> x;
-        v.push_back(x);
+        string a;
+        cin >> a;
+        if (!include(v, a))
+            v.push_back(a);
     }
 
     sort(v.begin(), v.end(), [](string a, string b) {
-        a.size() == b.size()
+        return a.size() == b.size()
             ? a < b
-            : 
+            : a.size() < b.size();
     });
 
-    string a = "hello";
-    cout<<a;
+    for (int i=0; i<v.size(); i++) {
+        cout << v[i] << "\n";
+    }
 
     return 0;
 }
