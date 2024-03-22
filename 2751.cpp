24529866de398a071e7cd13e26a578424500729c
Gnlow <gnlow@protonmail.com>
2024-03-22 14:04:25 +0900

    10989

--- 2751.cpp
+++ 2751.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(void) {
-    ios_base::sync_with_stdio(0);
     int n;
     cin >> n;
     
-    int v[10000];
-
-    for (int i=0; i<10000; i++) {
-        v[i] = 0;
-    }
+    vector<int> v;
 
     for (int i=0; i<n; i++) {
         int a;
         cin >> a;
-        v[a-1]++;
+        v.push_back(a);
     }
 
-    for (int i=0; i<10000; i++) {
-        for (int j=0; j<v[i]; j++) {
-            cout << i+1 << "\n";
-        }
+    sort(v.begin(), v.end());
+
+    for (int i=0; i<n; i++) {
+        cout << v.at(i) << "\n";
     }
 
     return 0;
